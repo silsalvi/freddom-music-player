6c551d1f94c0b926e2ed059fbc79515099bfcd7a
Silvestro Salvi <s.salvi@gematica.com>
2020-08-24 00:35:01 +0200

    Implementata UI music list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MusicListComponent } from './music-list/music-list.component';
-
+import { DataViewModule } from 'primeng/dataview';
+import { RippleModule } from 'primeng/ripple';
 @NgModule({
-  declarations: [
-    AppComponent,
-    MusicListComponent
-  ],
+  declarations: [AppComponent, MusicListComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    DataViewModule,
+    RippleModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
