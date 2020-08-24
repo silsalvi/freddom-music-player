a9e8a1c9179a208fd65c0b433a6400605812b18c
Silvestro Salvi <s.salvi@gematica.com>
2020-08-24 16:23:28 +0200

    Creato brani service

--- src/app/services/brani.service.ts
+++ src/app/services/brani.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@angular/core';
 import { brani } from '../mock-data';
 import { Brano } from '../models/brano.model';
-
+import { BehaviorSubject } from 'rxjs';
+import { Howl } from 'howler';
 @Injectable({
   providedIn: 'root',
 })
 export class BraniService {
   risultatiRicerca: Brano[] = brani;
+  braniSubject = new BehaviorSubject<Brano>(null);
+  brani$ = this.braniSubject.asObservable();
+  howl = new Howl({
+    src: [''],
+    format: ['mp3'],
+  });
+  mostraPlayer: boolean;
+  branoSelezionato: Brano;
+
+  /**
+   * Ritorna true se c'è già un brano in riproduzione
+   */
+  get isPlaying() {
+    return this.howl.playing();
+  }
   constructor() {}
 
   /**
@@ -15,5 +31,29 @@ export class BraniService {
    */
   getBraniMock() {
     return brani;
+  }
+
+  /**
+   * Riproduce un brano passato in input
+   */
+  riproduci(brano: Brano) {
+    this.branoSelezionato = brano;
+    this.mostraPlayer = true;
+    this.braniSubject.next(brano);
+    this.creaNuovoFlusso(brano);
+  }
+
+  /**
+   * Ritorna un nuovo oggetto di tipo Howl per riprodurre un nuovo brano
+   */
+  private creaNuovoFlusso(brano: Brano) {
+    if (this.howl.playing()) {
+      this.howl.pause();
+      this.howl.stop();
+    }
+    this.howl = new Howl({
+      src: [brano.path],
+    });
+    this.howl.play();
   }
 }
