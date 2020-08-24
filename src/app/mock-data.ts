c9268f890d8ae7e4ec1c9641c5732ad5e0dd361b
Silvestro Salvi <s.salvi@gematica.com>
2020-08-24 02:40:14 +0200

    Spostati brani in mock-data

---
+++ src/app/mock-data.ts
@@ -0,0 +1,20 @@
+import { MenuItem } from 'primeng/api';
+import { Brano } from './models/brano.model';
+export const menuItems: MenuItem[] = [];
+export const brani: Brano[] = [
+  {
+    durata: 500,
+    nome: 'Tik tok',
+    path: '',
+  },
+  {
+    durata: 500,
+    nome: 'Fear of the dark',
+    path: '',
+  },
+  {
+    durata: 500,
+    nome: 'Bohemian Rhapsody',
+    path: '',
+  },
+];
