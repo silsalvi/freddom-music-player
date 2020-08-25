ed3fcc6b44cf61a14cf945274f4d1404cdffa633
Silvestro Salvi <s.salvi@gematica.com>
2020-08-25 16:24:01 +0200

    Creato model autore e creata prop collaborazioni in brano model

--- src/app/models/brano.model.ts
+++ src/app/models/brano.model.ts
@@ -1,5 +1,36 @@
+import { Autore } from './autore.model';
 export class Brano {
   nome: string;
   path: string;
-  constructor() {}
+  collaborazioni?: Autore[];
+  autore: Autore;
+  dataUscita?: Date;
+  constructor(
+    nome: string,
+    path: string,
+    autore: Autore,
+    collaborazioni?: Autore[],
+    dataUscita?: Date
+  ) {
+    this.nome = nome;
+    this.path = path;
+    if (collaborazioni) {
+      this.collaborazioni = collaborazioni;
+    } else {
+      this.collaborazioni = [];
+    }
+    this.autore = autore;
+    this.dataUscita = dataUscita;
+  }
+
+  /**
+   * Ritorna una rappresentazione delle collaborazioni del brano sottoforma di stringa
+   * nella seguente forma:
+   * @example 'nomeArtista feat collab1,collab2 etc.'
+   */
+  collaborazioniToString() {
+    return `${this.autore.nomeArte} feat ${this.collaborazioni
+      .map((feat) => feat.nomeArte)
+      .join()}`;
+  }
 }
