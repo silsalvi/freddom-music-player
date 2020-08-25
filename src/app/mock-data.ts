83f4eb581ca141223fd5c4643fd72e7f7eb11064
Silvestro Salvi <s.salvi@gematica.com>
2020-08-25 16:24:28 +0200

    Aggiunto nuovo brano con featuring per testing funzionalità

--- src/app/mock-data.ts
+++ src/app/mock-data.ts
@@ -2,16 +2,29 @@ import { MenuItem } from 'primeng/api';
 import { Brano } from './models/brano.model';
 export const menuItems: MenuItem[] = [];
 export const brani: Brano[] = [
-  {
-    nome: 'Je so pazz',
-    path: 'assets/app-data/pino-daniele_je-so-pazzo.mp3',
-  },
-  {
-    nome: 'Fear of the dark',
-    path: 'assets/app-data/iron-maiden_fear-of-the-dark.mp3',
-  },
-  {
-    nome: 'Bohemian Rhapsody',
-    path: 'assets/app-data/queen_bohemian-rhapsody.mp3',
-  },
+  new Brano('Je so pazz', 'assets/app-data/pino-daniele_je-so-pazzo.mp3', {
+    nomeArte: 'Pino Daniele',
+  }),
+  new Brano(
+    'Fear of the dark',
+    'assets/app-data/iron-maiden_fear-of-the-dark.mp3',
+    {
+      nomeArte: 'Iron Maiden',
+    }
+  ),
+  new Brano(
+    'Bohemian Rhapsody',
+    'assets/app-data/queen_bohemian-rhapsody.mp3',
+    {
+      nomeArte: 'Queen',
+    }
+  ),
+  new Brano(
+    'Medellin',
+    'assets/app-data/gue-pequeno,lazza_medellin.mp3',
+    {
+      nomeArte: 'Gue Pequeno',
+    },
+    [{ nomeArte: 'Lazza' }]
+  ),
 ];
