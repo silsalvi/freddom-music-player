340d9ca8b0684570434323cf1c1fac3c7c5ca46f
Silvestro Salvi <sil.salvi@studenti.unina.it>
2021-01-24 21:19:35 +0100

    Inserite paginazione e fix player sticky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
-import { BraniService } from './services/brani.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,17 +7,8 @@ import { BraniService } from './services/brani.service';
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent implements OnInit {
-  constructor(
-    private primeNgConfig: PrimeNGConfig,
-    private braniService: BraniService
-  ) {}
+  constructor(private primeNgConfig: PrimeNGConfig) {}
 
-  get isPlaying() {
-    return this.braniService.isPlaying;
-  }
-  get mostraPlayer() {
-    return this.braniService.mostraPlayer;
-  }
   ngOnInit() {
     this.primeNgConfig.ripple = true;
   }
